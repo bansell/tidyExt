b4cca89205bdb56b6024a599f7c164b242433d7f
bansell <bansell@github.com>
2021-02-12 11:53:57 +1100

    update palette functions

--- R/brewer_GG_col.R
+++ R/brewer_GG_col.R
@@ -1,14 +1,16 @@
 #' Select R color brewer palette of type my_palette with nCols n colours
 #' @name brewer_GG_col
 #' @param my_palette An RColourBrewer palette name (see RColorBrewer::display.brewer.all())
-#' @param nCols Number of colours to select from palette. These will be evenly spaced across palette.
+#' @param nColours Number of colours to select from palette. These will be evenly spaced across palette.
 #' @export
-brewer_GG_col <- function(my_palette = 'Paired', nCols = 6){
-    scales::brewer_pal(palette = my_palette)(nCols)
-  }
+brewer_GG_col <-  function(nColours = 4, my_palette = 'Paired'){
+  print(scales::brewer_pal(palette = my_palette)(nColours))
+  scales::show_col(scales::brewer_pal(palette = my_palette)(nColours))
+}
 #'
 #' @examples
-#'brewer_GG_col('Blues', 3)
+#'brewer_GG_col(6,'Blues')
 #'
+
 
 
--- R/default_GG_col.R
+++ R/default_GG_col.R
@@ -1,12 +1,14 @@
 #' Select ggplot default colour palette of n. colours
 #' @name default_GG_col
-#' @param x N colours to select
+#' @param nColours N colours to display
 #' @export
-default_GG_col <- function(nCols = 5){
-  scales::hue_pal(h = c(0, 360) + 15, c = 100, l = 65, h.start = 0, direction = 1)(nCols)
+default_GG_col <- function(nColours = 3){
+  print(scales::hue_pal()(nColours))
+  scales::show_col(scales::hue_pal()(nColours))
   }
 #'
 #' @examples
 #' default_GG_col(3)
 #'
 #' default_GG_col(9)
+
