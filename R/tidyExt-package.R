95500eedb8a5c60f5f20c5aaf3f17e4b37d65b2f
bansell <bansell@github.com>
2021-07-07 19:07:41 +1000

    commit

---
+++ R/tidyExt-package.R
@@ -0,0 +1,58 @@
+#' tidyExt: a host of helper functions for the tidyverse, designed to
+#' overcome some common pain-points in plotting, data summarization and environment management.
+#'
+#'\href{https://github.com/bansell/tidyExt#tidyext-package}{\strong{tidyExt vignette}}
+#'
+#
+#' \strong{Functions for managing source code and global environment}
+#' \itemize{
+#' printScriptDir()
+#'
+#' fix_tidyverse_conflicts()
+#'}
+#'
+#' \strong{Data view helpers}
+#' \itemize{
+#' bighead()
+#'
+#' print_all()
+#'}
+#'
+#' \strong{Statistics helpers}
+#' \itemize{
+#' scale_this()
+#'
+#' sort_pct()
+#'}
+#'
+#' \strong{Faceted scatterplot colour recycling}
+#' \itemize{
+#' plot_cycle_cols()
+#'}
+#'
+#' \strong{GGplot2 colour helpers}
+#' \itemize{
+#' default_GG_col()
+#'
+#' brewer_GG_col()
+#'}
+#'
+#' \strong{GGplot2 shortcuts}
+#' \itemize{
+#' geom_boxjitter()
+#'
+#' geom_boxdodge()
+#'
+#' geom_smooth_lm()
+#'
+#' x_angle()
+#'
+#' no_legend()
+#'
+#' bottom_legend()
+#'}
+#'
+#' @title tidyExt
+#' @docType package
+#' @name tidyExt
+NULL
