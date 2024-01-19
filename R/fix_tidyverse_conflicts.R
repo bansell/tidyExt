4edfe88da36a5029deb2b8837d51d59ce5856c7d
bansell <bansell@github.com>
2024-01-19 12:50:56 +1100

    refactor

--- R/fix_tidyverse_conflicts.R
+++ R/fix_tidyverse_conflicts.R
@@ -1,24 +1,35 @@
 #' @export
 fix_tidyverse_conflicts <- function() {
-  tidyverse::tidyverse_conflicts() %>%
-    purrr::map2_df(., names(.), ~ dplyr::tibble(fun = .y, masker = .x[1], maskee = list(.x[-1]))) %>%
+
+  confl <- tidyverse::tidyverse_conflicts()
+
+  confl_tbl <- purrr::map2_df(confl,
+                              names(confl),
+                              ~ dplyr::tibble(fun = .y, masker = .x[1], maskee = list(.x[-1]))) %>%
     dplyr::mutate(fun = dplyr::if_else(stringr::str_detect(fun, '^%.+%$'),
                                        stringr::str_c('`', fun, '`'),
-                                       fun)) %>%
-    {
-      if(nrow(.)) {
-        tidyr::unnest(., maskee) %>%
-          dplyr::mutate_at(dplyr::vars(masker, maskee), ~ stringr::str_remove(., 'package:')) %>%
-          dplyr::filter(maskee %in% tidyverse::tidyverse_packages()) %>%
-          {
-            if(nrow(.)) {
-              with(., stringr::str_c(fun, ' <- ', maskee, '::', fun)) %>%
-                purrr::walk(~ {
-                  eval(parse(text = .), envir = .GlobalEnv)
-                  message(.)
-                })
-            }
-          }
-      }
+                                       fun))
+
+  {
+    if(nrow(confl_tbl) > 0) {
+
+      #for testing:
+      # confl_tbl$maskee[1] <- 'package:dplyr'
+
+      confl_tbl_tidyv <-  tidyr::unnest(confl_tbl, maskee) %>%
+        dplyr::mutate_at(dplyr::vars(masker, maskee), ~ stringr::str_remove(., 'package:')) %>%
+        dplyr::filter(maskee %in% tidyverse::tidyverse_packages())
+
+      {
+        if(nrow(confl_tbl_tidyv) > 0) {
+          to_assign <- with(confl_tbl_tidyv, stringr::str_c(fun, ' <- ', maskee, '::', fun))
+
+          purrr::walk(to_assign, ~ {
+            eval(parse(text = .), envir = .GlobalEnv)
+            message(.)
+          })
+        }
+        }
+    }
     }
 }
